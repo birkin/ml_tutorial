2ce7226447f4562d7855416cf91d046f83606afa
Birkin James Diana <birkin.diana@gmail.com>
2023-04-09 02:55:11 -0400

    starts viewing some images.

--- ml_code.py
+++ ml_code.py
@@ -8,13 +8,8 @@ from torch import nn
 from torchvision import datasets, transforms, models
 
 
-# import torchvision  # must be imported before torch to avoid circular import error  ¯\_(ツ)_/¯
-# import torch
-# import torch.nn.functional as F
-# from torch import nn
+## setup  -----------------------------------------------------------
 
-
-## setup logging ----------------------------------------------------
 logging.basicConfig(
     level=logging.DEBUG,
     format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
@@ -22,19 +17,18 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 log.debug( 'logging ready' )
 
-
 ## hyper parameters
 epochs = 10
 batch_size = 32
 learning_rate = 0.0001
-
 
 ## for GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 log.debug( f'device: {device}' )
 
 
-## Prepare the dataset
+## Prepare the dataset ----------------------------------------------
+
 transform_train = transforms.Compose(
     [   transforms.Resize((224,224)),
         transforms.RandomHorizontalFlip(),
@@ -72,8 +66,6 @@ transform, ``Compose(
 )``
 """
 
-
-## next...
 train_dataset = datasets.ImageFolder('../flower_photos/train', transform=transform_train)
 val_dataset = datasets.ImageFolder('../flower_photos/test', transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -81,3 +73,42 @@ val_loader = torch.utils.data.DataLoader(val_dataset, batch_size = batch_size, s
 
 log.debug( f'train_dataset, ``{train_dataset}``' )
 log.debug( f'val_dataset, ``{val_dataset}``' )
+
+
+## view some images -------------------------------------------------
+
+def im_convert( tensor ):
+  """ To visualize some sample images. """
+  image = tensor.cpu().clone().detach().numpy()
+  image = image.transpose(1, 2, 0)
+  image = image * np.array((0.5, 0.5, 0.5)) + np.array((0.5, 0.5, 0.5))
+  image = image.clip(0, 1)
+  return image
+
+classes = ('daisy', 'dandelion', 'roses', 'sunflowers', 'tulips')
+
+# dataiter = iter(train_loader)
+# images, labels = dataiter.next()
+# fig = plt.figure(figsize=(25, 4))
+# for idx in np.arange(20):
+#     ax = fig.add_subplot(2, 10, idx+1, xticks=[], yticks=[])
+#     plt.imshow(im_convert(images[idx]))
+#     ax.set_title( classes[labels[idx].item()] )
+
+
+dataiter = iter(train_loader)
+# images, labels = dataiter.next()
+images, labels = next( dataiter )
+
+"""
+Results in error...
+```AttributeError: '_SingleProcessDataLoaderIter' object has no attribute 'next'```
+See <https://github.com/CompVis/metric-learning-divide-and-conquer/issues/12#issuecomment-733830625>`
+"""
+
+fig = plt.figure(figsize=(25, 4))
+
+for idx in np.arange(20):
+  ax = fig.add_subplot(2, 10, idx+1, xticks=[], yticks=[])
+  plt.imshow(im_convert(images[idx]))
+  ax.set_title(classes[labels[idx].item()])
